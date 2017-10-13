0462e752aa6f975dc37c9232b2802fff61291a8b
Aruna kadugu <arunakumari.7k@gmail.com>
2017-10-13 14:30:28 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int count;
+        int num7 =0;
+        int num17 =0;
+        int num23 =0;
+        int usernumber;
+        boolean exit = true;
+        System.out.println("enter the number:");
+        int num = sc.nextInt();
+
+
+
+        System.out.println("enter the number  count of multiplies:");
+        usernumber = sc.nextInt();
+       // while(count <= n)
+        for(count  = 1; count <= num; count++)
+
+        {
+
+            if ((count % 3 != 0) && (count % 5 != 0))
+                System.out.println(count);
+            else if ((count % 3 == 0) && (count % 5 == 0))
+                System.out.println(" fizzbuzz");
+            else if (count % 3 == 0)
+                System.out.println(" fizz");
+            else if (count % 5 == 0)
+                System.out.println(" buzz");
+            if(count %7 ==0)
+             num7++;
+            if(count %17 ==0)
+              num17++;
+            if(count % 23 == 0)
+                num23++;
+        }
+
+        System.out.println("number of multiples of 7 in " + num + " : " + num7);
+        System.out.println("number of multiples of "+ usernumber + " in "+num+" : "+usernumber);
+    }
+}
